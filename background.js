ef9805b51b7a16ce78435d89b5d1535e599a5e28
MagicSakuraD <ckgoforit@outlook.com>
2023-05-04 23:02:23 +0800

    jobapply extension

---
+++ background.js
@@ -0,0 +1,20 @@
+console.log("hello world");
+// async function fetchData() {
+//   const url =
+//     "https://restapi.amap.com/v3/weather/weatherInfo?key=22a3efc9d5db15f049f0720cb12f6e95&city=310115";
+//   const options = {
+//     method: "GET",
+//   };
+
+//   try {
+//     const response = await fetch(url, options);
+//     const result = await response.json();
+//     console.log("this is output", result);
+//     document.getElementById("weather").innerHTML =
+//       result["lives"][0]["weather"];
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// fetchData();
---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("欢迎使用");
+// async function jobapply() {
+//   // 在jobapply函数中，获取class为btn的元素集合，然后模拟点击第一个元素
+//   btn = document.getElementsByClassName("btn");
+//   try {
+//     console.log(btn.length);
+//     await btn[0].click();
+//   } catch (err) {
+//     console.log(err, "失败");
+//   }
+// }
+// script.js
+function jobapply() {
+  alert("Button clicked!");
+}
